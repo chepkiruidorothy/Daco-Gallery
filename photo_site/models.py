41becdac43beda474f29563f7681f09295bab6ec
orichcasperkevin <orichcaspertheghost@gmail.com>
2023-05-21 14:19:42 +0300

    images

--- photo_site/models.py
+++ photo_site/models.py
@@ -1,27 +1,39 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 # Create your models here.
-
+from .resize import resize_image,get_dominantcolor
 
 class Category(models.Model):
-    name = models.CharField(max_length = 20)
-    slug = models.SlugField(max_length=100, unique=True, null=True)
+	name = models.CharField(max_length = 20)
+	slug = models.SlugField(max_length=100, unique=True, null=True)
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super(Category, self).save(*args, **kwargs)
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
 
 class Shoot(models.Model):
-    person = models.CharField(max_length = 20)
+	person = models.CharField(max_length = 20)
 
-    def __str__(self):
-        return self.person
+	def __str__(self):
+		return self.person
 
 class Gallery(models.Model):
-    image = models.ImageField(upload_to="images", blank=True, null=True)
-    category = models.ForeignKey(Category, on_delete = models.CASCADE)
-    shoot = models.ForeignKey(Shoot, on_delete = models.CASCADE, null=True)
+	image = models.ImageField(upload_to="images", blank=True, null=True)
+	category = models.ForeignKey(Category, on_delete = models.CASCADE)
+	shoot = models.ForeignKey(Shoot, on_delete = models.CASCADE, null=True)
+	dominant_color = models.CharField(default='(228, 237, 230)',max_length=20)
+	height = models.IntegerField(default=200)
+	width = models.IntegerField(default=200)
+
+
+	def save(self, *args, **kwargs):   
+		self.image = resize_image(self.image)
+		self.dominant_color = get_dominantcolor(self.image)
+		self.height = self.image.height
+		self.width = self.image.width
+		#normal save
+		super(Gallery, self).save(*args, **kwargs)#normal save
---
+++ photo_site/resize.py
@@ -0,0 +1,44 @@
+
+from io import BytesIO
+from django.core.files import File
+from PIL import Image
+from colorthief import ColorThief
+
+
+SQUARE_IMG_SIZE = (600,600)
+LANDSCAPE_IMG_SIZE = (600,400)
+PORTRAIT_IMG_SIZE = (480,600)
+
+
+def resize_image(image):
+    """
+        resizes and reduces image quality.
+    """
+    im = Image.open(image)
+    im_width, im_height = im.size
+    size = None
+
+    if im_width == im_height:
+        #image is a square
+        size = SQUARE_IMG_SIZE
+    if im_width > im_height:
+        #image is landscape
+        size = LANDSCAPE_IMG_SIZE
+    if im_width < im_height:
+        # image is portrait
+        size = PORTRAIT_IMG_SIZE
+
+
+    im = im.convert('RGB') # convert mode to RGB so we an use JPEG later
+    im = im.resize(size, Image.ANTIALIAS)  # resize image
+    thumb_io = BytesIO() # create a BytesIO object
+    im.save(thumb_io, 'JPEG', quality=80, progressive=True, optimize=True) # save image to BytesIO object
+    new_image = File(thumb_io, name=image.name) # create a django friendly File object
+
+    return new_image
+
+def get_dominantcolor(image):
+    color_thief = ColorThief(image)
+    # get the dominant color
+    dominant_color = color_thief.get_color(quality=1)
+    return dominant_color
